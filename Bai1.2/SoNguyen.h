343d5de195574128212112b18a00dc2d4c3181c9
Nguyen Quoc Chau <chauquocnguyen.cun1@gmail.com>
2022-07-21 23:23:48 +0700

    update

---
+++ Bai1.2/SoNguyen.h
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define max 50
+void nhapMang(int a[], int &n);
+void xuatMang(int a[],int n);
+void xoa1PhanTu(int a[], int &n, int ViTriXoa);
+void locTrung(int a[],int n);
+bool kiemTraNguyenTo(int n);
+void xuatNguyenTo(int a[], int n);
+bool kiemTraChinhPhuong(int n);
+void xuatChinhPhuong(int a[],int n);
+int tongGiaTri(int a[],int n);
+void trungBinhMang(int a[],int n);
+void soLonNhat(int a[],int n);
+bool kiemTraTangDan(int a[],int n);
+void kQKiemTraTangDan(int a[],int n);
+void sapXepTangDan(int a[],int n);
+void sapXepGiamDan(int a[],int n);
