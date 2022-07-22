607527db8dbe17e0b39a2925730df53092d40a48
Nguyễn Quốc Châu <84402319+nguyenquocchauit@users.noreply.github.com>
2022-07-22 22:54:56 +0700

    Add files via upload

---
+++ Bai1.5/TronMang.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include "TronMang.h"
+void xuatMang(int arr[],int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		printf("%d\t ",arr[i]);
+	}
+}
+void tronMang(int a[],int b[], int c[],int n,int m,int &k)
+{
+	int i=0,j=0;
+	while((i<n) && (j<m))
+	{
+		if(a[i]<b[j])
+		{
+			c[k++]=a[i++];
+		}
+		else
+		{
+			c[k++]=b[j++];
+		}
+	}
+	while(i<n) // duyet den cuoi phan tu mang a
+	{
+      c[k++]=a[i++];
+   	}
+   	while(j<m) // duyet den cuoi phan tu mang b
+   	{
+      c[k++]=b[j++];
+   	}
+}
---
+++ Bai1.5/TronMang.h
@@ -0,0 +1,3 @@
+void xuatMang(int arr[],int n);
+void tronMang(int a[],int b[], int c[],int n,int m,int &k);
+
